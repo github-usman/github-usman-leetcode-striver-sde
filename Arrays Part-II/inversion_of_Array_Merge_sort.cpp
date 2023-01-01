b83eb6dff207466312460d289cf9a7a33aaf9435
Usman_Ali_Ansari <usmantoyou@gmail.com>
2023-01-01 14:15:26 +0530

    updae the Array second question with optimized solution

--- Arrays Part-II/inversion_of_Array_Merge_sort.cpp
+++ Arrays Part-II/inversion_of_Array_Merge_sort.cpp
@@ -1,49 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long int
-
-////////////***First Approach***////////////
-void driver(){
-
-    ll n;
-    cin>>n;
-
-    vector<ll> arr;
-    int temp;
-
-    for(int i = 0;i<n;i++){
-        cin>>temp;
-        arr.push_back(temp);
-        
-    }
-
-    int cnt = 0;
-    for(int i = (n-1);i>=0;i--){
-        for(int j = 0;j<=i;j++){
-            if(arr[j]>arr[i]){
-                cnt++;
-            }
+int merge(int arr[], int temp[], int left, int mid, int right)
+{
+    int inv_count = 0;
+    int left1 = left;
+    int j = mid;
+    int left2 = left;
+    while ((left1 <= mid - 1) && (j <= right))
+    {
+        if (arr[left1] <= arr[j])
+        {
+            temp[left2++] = arr[left1++];
+        }
+        else
+        {
+            temp[left2++] = arr[j++];
+            inv_count = inv_count + (mid - left1);
         }
     }
 
+    while (left1 <= mid - 1)
+        temp[left2++] = arr[left1++];
 
-    // we can solve it by using merge sort
-    cout<<cnt<<endl;
+    while (j <= right)
+        temp[left2++] = arr[j++];
+
+    for (left1 = left; left1 <= right; left1++)
+        arr[left1] = temp[left1];
+
+    return inv_count;
 }
 
-//////////////***Main Function***//////////////
-int32_t main(){
+int merge_Sort(int arr[], int temp[], int left, int right)
+{
+    int mid, inv_count = 0;
+    if (right > left)
+    {
+        mid = left + (right - left) / 2;
 
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+        inv_count += merge_Sort(arr, temp, left, mid);
+        inv_count += merge_Sort(arr, temp, mid + 1, right);
 
-    int t;
-    cin>>t;
+        inv_count += merge(arr, temp, left, mid + 1, right);
+    }
+    return inv_count;
+}
 
-    while(t--)
-    driver();
+int main()
+{
+    int arr[] = {5, 3, 2, 1, 4};
+    int n = 5;
+    int temp[n];
+    int ans = merge_Sort(arr, temp, 0, n - 1);
+    cout << "The total inversions are " << ans << endl;
+    for (int left1 = 0; left1 < n; left1++)
+    {
+        cout << arr[left1] << " ";
+    }
 
     return 0;
 }
