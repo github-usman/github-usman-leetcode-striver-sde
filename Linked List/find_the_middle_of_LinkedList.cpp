a90db0d269600a6b7a3837887bebe4e596504d79
Usman_Ali_Ansari <usmantoyou@gmail.com>
2023-01-07 23:47:27 +0530

    Optimizing the code fo the linked list section

--- Linked List/find_the_middle_of_LinkedList.cpp
+++ Linked List/find_the_middle_of_LinkedList.cpp
@@ -1,80 +1,83 @@
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+#include <stdlib.h>
 
 using namespace std;
-// create inbuilt node
- struct ListNode {
-     int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
-// value insertion
-void insertAtTail(ListNode* &head, int data){
-    ListNode* n = new ListNode(data);
-    ListNode* temp = head;
 
-    if(head == NULL){
-        head = n;
-        return;
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
     }
-    while(temp->next != NULL){
-        temp = temp->next;
+};
+
+int size(node *head)
+{
+    int ans = 0;
+    while (head != NULL)
+    {
+        ans++;
+        head = head->next;
     }
-    temp->next = n;
-} 
-// to display what is happening
-int display(ListNode* head){
-    int cnt = 0;
-    ListNode* temp = head;
-    while(temp != NULL){
-        cnt++;
-        cout<<temp->val<<" ";
-        temp = temp->next;
-    }
-    return cnt;
+    return ans;
+}
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+
+    n->next = head;
+    head = n;
 }
 
-ListNode* middleElement(ListNode* head,int cnt){
-    int mid = (cnt/2)+1;
-    int i = 1;
-    ListNode* temp = head;
-
-    while(1){
-
-        if(mid == i){
-            break;
-        }
-        i++;
-        temp = temp->next;
-
+void display(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
     }
-    return temp;
 }
+// hair turtle
+node *midNode(node *head)
+{
+    node* temp1 = head;
+    node* temp2 = head->next;
 
-int main(){
+    while(temp2 != NULL && temp2->next != NULL){
+        temp1 = temp1->next;
+        temp2 = temp2->next->next;
+    }
 
-      int n;
-      cin>>n;
-    
-        ListNode* head = NULL;
-        while(n--)
-        {
-            int temp;
-            cin>>temp;
-            insertAtTail(head,temp);
-        }
-        
+    if(temp2 == NULL){
+        return temp1;
+    }
 
-        int cnt = display(head);
-        cout<<cnt<<endl;
-        
-        ListNode* ans = middleElement(head,cnt);
-        cout<<ans->val<<endl;
-        
-      
-      
+    return temp1->next;
+}
+// find mid element of the linked list
 
-      return 0;
+int main()
+{
+
+    node *head = NULL;
+    insertAtHead(head, 1);
+    insertAtHead(head, 2);
+    insertAtHead(head, 3);
+    insertAtHead(head, 4);
+    insertAtHead(head, 5);
+    // insertAtHead(head, 6);
+
+    display(head);
+    cout << endl;
+    // int val = size(head);
+   node* value = midNode(head);
+   cout<<value->data<<endl;
+   
+
+    return 0;
 }
--- Linked List/reverse_a_LinkedList.cpp
+++ Linked List/reverse_a_LinkedList.cpp
@@ -1,57 +1,76 @@
 #include<iostream>
 #include<stdlib.h>
-//  * Definition for singly-linked list.
- struct ListNode {
-     int val;
-     ListNode *next;
-   ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
+
+using namespace std;
 
 
+struct node{
+    int data;
+    node* next;
 
-   struct node2 {
-      int data;
-      node2 *next;
-      node2(int val){
+    node(int val){
         data = val;
         next = NULL;
-      }
-  };
+    }
+};
 
-void reversedLinkedList(node2* &head2,ListNode* head){
-    ListNode* temp = head;
-    while(temp != NULL){
-        node2* n = new node2(temp->val);
-        n->next = head2;
-        head2 = n;
+void  insertAtTail(node* &head,int val){
+    node* n = new node(val);
+
+    if(head == NULL){
+        head = n;
+        return ;
+    }
+    node* temp = head;
+    while(temp->next != NULL){
         temp = temp->next;
     }
-
+    temp->next = n;
+    
 }
 
-void copyTheValue(ListNode* &head,node2* head2){
-    ListNode* temp = head;
-    node2* temp2 = head2;
-    while(temp != NULL){
-        temp->val = temp2->data;
-        temp = temp->next;
-        temp2 = temp2->next; 
+
+
+node* reversedLinkedList(node* &head){
+    node* move = NULL;
+    node* temp;
+
+    while(head != NULL){
+        temp = head->next;  //for doing iteration
+        head->next = move; //for giving the address of the new node 
+        move = head; // putting the value from original to new node
+        head = temp; //move forward the iteration one value next
     }
-}
 
+    return move;
+}
+void display(node* head){
+    node* temp = head;
+    // cout<<"head->";
+    while(temp != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
 
 int main(){
 
+    node* head = NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    insertAtTail(head,4);
+    insertAtTail(head,5);
+    insertAtTail(head,6);
+    display(head);
+    head = reversedLinkedList(head);
+    display(head);
+
+
    
-        // reverse using another linked list
-        node2* head2 = NULL;
-        ListNode*  head = NULL;
-        reversedLinkedList(head2,head);
-        // ListNode* reversed = NULL;
-        copyTheValue(head,head2);
-  
-    
+
+
+
     return 0;
 }
